17e7d0966703dbc3b705db62c618df333ffca3df
Dylan Halperin <dylan@dylemma.io>
2015-10-27 23:56:30 -0400

    Add prepend ops to the Chain class

--- core/src/main/scala/io/dylemma/xml/Chain.scala
+++ core/src/main/scala/io/dylemma/xml/Chain.scala
@@ -41,6 +41,9 @@ object ChainSyntax {
 		}
 	}
 
+	val ~ = Chain
+	type ~[A, B] = Chain[A, B]
+
 	implicit class anyChainAssoc[A](head: A) {
 		def ~[B](tail: B) = Chain(head, tail)
 	}
@@ -48,10 +51,12 @@ object ChainSyntax {
 	/** Typeclass that witnesses that a type `T` is not a `Chain`. */
 	sealed trait IsNotAChain[T]
 	final class AnyIsNotAChain[T] extends IsNotAChain[T]
-	final class ChainIsNotAChainForAmbiguity[T <: Chain[_, _]] extends IsNotAChain[T]
 	implicit def anythingIsNotAChain[T]: IsNotAChain[T] = new AnyIsNotAChain[T]
-	implicit def chainIsNotAChainFormAmbiguity[T <: Chain[_, _]]: IsNotAChain[T] = ??? //new ChainIsNotAChainForAmbiguity[T]
-	implicit def chainIsNotAChainForAmbiguity2[T <: Chain[_, _]]: IsNotAChain[T] = ??? //new ChainIsNotAChainForAmbiguity[T]
+
+	// provide two separate implicits that say a Chain is not a Chain, creating
+	// ambiguity so that the compiler can't prove that a Chain is not a Chain.
+	implicit def chainIsNotAChainForAmbiguity[T <: Chain[_, _]]: IsNotAChain[T] = ???
+	implicit def chainIsNotAChainForAmbiguity2[T <: Chain[_, _]]: IsNotAChain[T] = ???
 
 	/** ChainConcat implementation that handles concatenation of a two-item chain
 		* by appending the head item, then appending the tail item.
@@ -88,11 +93,69 @@ object ChainSyntax {
 		new InductiveChainConcat(pstConcat)
 	}
 
+	/** Typeclass that knows how to prepend a single value in front of a chain.
+		*
+		* @tparam P The prefix value type
+		* @tparam C The chain type
+		* @tparam R The result chain's type
+		*/
+	trait ChainPrepend[P, C <: Chain[_, _], R <: Chain[_, _]]{
+		def prepend(value: P, chain: C): R
+	}
+
+	/** Introduces the `prepend` and `~:` operations on chains when there
+		* is an appropriate ChainPrepend instance available.
+		*/
+	implicit class ChainPrependOps[C <: Chain[_, _]](chain: C){
+		def prepend[P, R <: Chain[_, _]](value: P)(implicit prepender: ChainPrepend[P, C, R]): R = {
+			prepender.prepend(value, chain)
+		}
+		def ~:[P, R <: Chain[_, _]](value: P)(implicit prepender: ChainPrepend[P, C, R]): R = {
+			prepend(value)
+		}
+	}
+
+	/** ChainPrepend implementation that handles prepends to a two-item chain
+		* by creating a three-item chain.
+		*
+		* P ~: (A ~ B) = P ~ A ~ B
+		*/
+	class SimpleChainPrepend[P, A: IsNotAChain, B] extends ChainPrepend[P, Chain[A, B], Chain[Chain[P, A], B]]{
+		def prepend(value: P, chain: Chain[A, B]) = {
+			Chain(Chain(value, chain.head), chain.tail)
+		}
+	}
+	implicit def provideSimpleChainPrepend[P, A: IsNotAChain, B]: ChainPrepend[P, Chain[A, B], Chain[Chain[P, A], B]] = {
+		new SimpleChainPrepend[P, A, B]
+	}
+
+	/** ChainPrepend implementation that handles prepends to longer chains by
+		* delegating to another ChainPrepend that can handle prepends to the
+		* chain's head (this process will recurse until the head is a two-item
+		* list, at which point it will use the SimpleChainPrepend).
+		*
+		*  P ~: (complex ~ T) = (P ~: complex) ~ T
+		*
+		* @param pcPrepender
+		* @tparam P The prefix value type
+		* @tparam C The chain's head (an inner chain)
+		* @tparam PC The result type of prepending a `P` to a `C`
+		* @tparam T The chain's tail type
+		*/
+	class InductiveChainPrepend[P, C <: Chain[_, _], PC <: Chain[_, _], T](implicit pcPrepender: ChainPrepend[P, C, PC])
+		extends ChainPrepend[P, Chain[C, T], Chain[PC, T]] {
+		def prepend(value: P, chain: Chain[C, T]) = {
+			Chain(pcPrepender.prepend(value, chain.head), chain.tail)
+		}
+	}
+	implicit def provideInductiveChainPrepend[P, C <: Chain[_, _], PC <: Chain[_, _], T](
+		implicit pcPrepender: ChainPrepend[P, C, PC]): ChainPrepend[P, Chain[C, T], Chain[PC, T]] = {
+		new InductiveChainPrepend[P, C, PC, T]
+	}
 }
 
 object ChainSyntaxTesting extends App {
 	import ChainSyntax._
-	val ~ = Chain
 
 	val a = 1 ~ "hello" ~ true ~ List(1,2,3) ~ Option(3) ~ Map(1->'c', 2->'d')
 	val b = 5.234 ~ false
@@ -108,4 +171,8 @@ object ChainSyntaxTesting extends App {
 		case i ~ s ~ b ~ list ~ opt ~ map ~ d ~ t =>
 	}
 
+	val pa = 7.3 ~: a
+	val pb = 7.3 ~: b
+	println(pa)
+	println(pb)
 }
