e9e47c856082f9300b11f3dde7bf959e9bd8321e
Dylan Halperin <dylan@dylemma.io>
2015-10-29 18:43:03 -0400

    Implement Matcher trait that uses Chains to combine

---
+++ core/src/main/scala/io/dylemma/xml/MatchResult.scala
@@ -0,0 +1,76 @@
+package io.dylemma.xml
+
+import io.dylemma.xml.ChainSyntax._
+
+sealed trait MatchResult[+T]
+object MatchResult {
+	case object Ok extends MatchResult[Nothing]
+	case class SingleValue[T](value: T) extends MatchResult[T]
+	case class ChainValues[T <: Chain[_, _]](values: T) extends MatchResult[T]
+}
+
+trait MatchResultCombiner[T1 <: MatchResult[_], T2 <: MatchResult[_], R <: MatchResult[_]]{
+	def combine(result1:T1, result2: T2): R
+}
+
+object MatchResultCombiner {
+	import MatchResult._
+
+	// Ok + <X> = <X>
+	class OkAnyCombiner[R <: MatchResult[_]] extends MatchResultCombiner[Ok.type, R, R] {
+		def combine(ok: Ok.type, result: R) = result
+	}
+	implicit def provideOkAnyCombiner[R <: MatchResult[_]]: OkAnyCombiner[R] = new OkAnyCombiner[R]
+	// note: avoid introducing an `X + Ok` combiner as it would cause ambiguity for the `Ok + Ok` case
+
+	// Single(s) + Ok = Single(s)
+	class SingleOkCombiner[S] extends MatchResultCombiner[SingleValue[S], Ok.type, SingleValue[S]] {
+		def combine(single: SingleValue[S], ok: Ok.type) = single
+	}
+	implicit def provideSingleOkCombiner[S]: SingleOkCombiner[S] = new SingleOkCombiner[S]
+
+	// Single(s) + Single(t) = Chain(s ~ t)
+	class SingleSingleCombiner[S, T] extends MatchResultCombiner[SingleValue[S], SingleValue[T], ChainValues[S ~ T]]{
+		def combine(left: SingleValue[S], right: SingleValue[T]) = ChainValues(left.value ~ right.value)
+	}
+	implicit def provideSingleSingleCombiner[S, T]: SingleSingleCombiner[S, T] = new SingleSingleCombiner[S, T]
+
+	// Single(s) + Chain(c) = Chain(s ~: c)
+	class SingleChainCombiner[S, C <: Chain[_, _], R <: Chain[_, _]](implicit prepender: ChainPrepend[S, C, R])
+		extends MatchResultCombiner[SingleValue[S], ChainValues[C], ChainValues[R]]{
+		def combine(left: SingleValue[S], right: ChainValues[C]) = {
+			ChainValues(left.value ~: right.values)
+		}
+	}
+	implicit def provideSingleChainCombiner[S, C <: Chain[_, _], R <: Chain[_, _]](
+		implicit prepender: ChainPrepend[S, C, R]
+	): SingleChainCombiner[S, C, R] = new SingleChainCombiner[S, C, R]
+
+	// Chain + Ok = Chain
+	class ChainOkCombiner[C <: Chain[_, _]] extends MatchResultCombiner[ChainValues[C], Ok.type, ChainValues[C]]{
+		def combine(chain: ChainValues[C], ok: Ok.type): ChainValues[C] = chain
+	}
+	implicit def provideChainOkCombiner[C <: Chain[_, _]]: ChainOkCombiner[C] = new ChainOkCombiner[C]
+
+	// Chain(c) + Single(s) = Chain(c ~ s)
+	class ChainSingleCombiner[C <: Chain[_, _], S] extends MatchResultCombiner[ChainValues[C], SingleValue[S], ChainValues[C ~ S]] {
+		def combine(result1: ChainValues[C], result2: SingleValue[S]): ChainValues[C ~ S] = {
+			ChainValues(result1.values ~ result2.value)
+		}
+	}
+	implicit def provideChainSingleCombiner[C <: Chain[_, _], S]: ChainSingleCombiner[C, S] = new ChainSingleCombiner[C, S]
+
+	// Chain(a) + Chain(b) = Chain(a ++ b)
+	class ChainChainCombiner[A <: Chain[_, _], B <: Chain[_, _], R <: Chain[_, _]](implicit chainConcat: ChainConcat[A, B, R])
+		extends MatchResultCombiner[ChainValues[A], ChainValues[B], ChainValues[R]] {
+		def combine(left: ChainValues[A], right: ChainValues[B]): ChainValues[R] = {
+			ChainValues(left.values ++ right.values)
+		}
+	}
+	implicit def provideChainChainCombiner[A <: Chain[_, _], B <: Chain[_, _], R <: Chain[_, _]](
+		implicit chainConcat: ChainConcat[A, B, R]
+	): ChainChainCombiner[A, B, R] = new ChainChainCombiner[A, B, R]
+
+	val okok = implicitly[MatchResultCombiner[Ok.type, Ok.type, Ok.type]]
+
+}
---
+++ core/src/main/scala/io/dylemma/xml/Matcher.scala
@@ -0,0 +1,147 @@
+package io.dylemma.xml
+
+import javax.xml.namespace.QName
+import scala.language.implicitConversions
+
+case class OpenTag(name: QName, attrs: Map[QName, String])
+
+object TagMatcherSemantics extends MatcherSemantics[OpenTag] {
+	import event._
+
+	def tag(name: String) = Matcher.predicate {
+		case OpenTag(Name(n), _) => name == n
+	}
+	def attr(name: String) = Matcher { tag =>
+		val qname = new QName(name)
+		tag.attrs get qname
+	}
+
+	def main(args: Array[String]) {
+		val matcher1 = tag("finding") & attr("id") & attr("stuff")
+		val matcher2 = tag("foo")
+		val matcher3 = tag("toolResult") & attr("id")
+
+		val listMatcher = matcher1 / matcher2 / matcher3
+
+		def Tag(name: String, attrs: (String, String)*) = {
+			val qName = new QName(name)
+			val attrsMap = (for {(k, v) <- attrs} yield (new QName(k), v)).toMap
+			OpenTag(qName, attrsMap)
+		}
+
+		val tag1 = Tag("finding", "id" -> "123", "stuff" -> "foof", "things" -> "blarg")
+		val tag2 = Tag("foo", "id" -> "asdf")
+		val tag3 = Tag("toolResult", "id" -> "abc", "stuff" -> "eokjd")
+		val tag4 = Tag("things", "content" -> "bad")
+		val tag5 = Tag("thing", "format" -> "markdown")
+
+		val tagsList = tag1 :: tag2 :: tag3 :: tag4 :: tag5 :: Nil
+		val result = listMatcher(tagsList)
+		println(result)
+
+		val wrongList = tag1 :: tag2 :: tag5 :: Nil
+		println(listMatcher(wrongList))
+
+		val wrongList2 = tag1 :: Nil
+		println(listMatcher(wrongList2))
+	}
+}
+
+trait MatcherSemantics[Input] {
+
+	trait Matcher[M <: MatchResult[_]] {
+		def apply(input: Input): Option[M]
+
+		def &[N <: MatchResult[_], R <: MatchResult[_]](that: Matcher[N])(
+			implicit combiner: MatchResultCombiner[M, N, R]
+		): Matcher[R] = new CombinedMatcher(this, that)
+
+		def asListMatcher: ListMatcher[M] = new SingleListMatcher[M](this)
+	}
+
+	implicit def matcherAsListMatcher[M <: MatchResult[_]](matcher: Matcher[M]): ListMatcher[M] = matcher.asListMatcher
+
+	private class CombinedMatcher[A <: MatchResult[_], B <: MatchResult[_], R <: MatchResult[_]]
+		(a: Matcher[A], b: Matcher[B])
+			(implicit combiner: MatchResultCombiner[A, B, R])
+		extends Matcher[R] {
+		def apply(input: Input): Option[R] = {
+			for {
+				resultA <- a(input)
+				resultB <- b(input)
+			} yield combiner.combine(resultA, resultB)
+		}
+	}
+
+	object Matcher {
+		import MatchResult._
+
+		def apply[A](f: Input => Option[A]): Matcher[SingleValue[A]] = new Matcher[SingleValue[A]] {
+			def apply(elem: Input) = f(elem) map {SingleValue(_)}
+		}
+		def predicate(f: Input => Boolean): Matcher[Ok.type] = new Matcher[Ok.type] {
+			def apply(elem: Input) = if (f(elem)) Some(Ok) else None
+		}
+	}
+
+	trait ListMatcher[M <: MatchResult[_]] {
+		/** Consumes some number of inputs from the beginning of the `inputs` list,
+			* optionally returning a result value and the remaining inputs as a pair.
+			*
+			* @param inputs
+			* @return
+			*/
+		def apply(inputs: List[Input]): Option[(M, List[Input])]
+
+		def /[N <: MatchResult[_], MN <: MatchResult[_]](nextMatcher: Matcher[N])(
+			implicit combiner: MatchResultCombiner[M, N, MN]): ListMatcher[MN] = {
+			new InductiveListMatcher(this, nextMatcher.asListMatcher)
+		}
+	}
+
+	class SingleListMatcher[H <: MatchResult[_]](headMatcher: Matcher[H]) extends ListMatcher[H] {
+		def apply(inputs: List[Input]) = inputs match {
+			case Nil => None
+			case head :: tail =>
+				val headMatch = headMatcher(head)
+				headMatch.map(_ -> tail)
+		}
+	}
+
+	class InductiveListMatcher[A <: MatchResult[_], B <: MatchResult[_], AB <: MatchResult[_]]
+		(leadMatcher: ListMatcher[A], secondMatcher: ListMatcher[B])
+			(implicit combiner: MatchResultCombiner[A, B, AB])
+		extends ListMatcher[AB] {
+
+		def apply(inputs: List[Input]) = {
+			for {
+				(leadMatch, leadTail) <- leadMatcher(inputs)
+				(secondMatch, remaining) <- secondMatcher(leadTail)
+			} yield {
+				val combinedMatch = combiner.combine(leadMatch, secondMatch)
+				combinedMatch -> remaining
+			}
+		}
+	}
+
+	/*
+	Matcher[A] / Matcher[B] / Matcher[C]
+	thingA :: thingB :: thingC :: otherThings :: Nil
+	= (Matcher[A] / Matcher[B]) / Matcher[C]
+	= Matcher[AB] / Matcher[C]
+	= Matcher[ABC]
+
+	(Input, Matcher[A])
+	(Input, Matcher[B])
+
+	def apply(inputs: List[Input]) = inputs match {
+		case Nil => fail
+		case head :: tail =>
+			val headResult = headMatcher(head)
+			tailMatcher(tail) match {
+				case tailResult + remainingStuff =>
+					val ab = combine(headResult, tailResult)
+					tailResult.fold(ab)
+
+	 */
+}
