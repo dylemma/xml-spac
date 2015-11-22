6e404c87b8a53211eb0d5946654b8382911a67a0
Dylan Halperin <dylan@dylemma.io>
2015-11-22 10:35:21 -0500

    rename ResultCombiner->ContextCombiner and move to its own file

--- core/src/main/scala/io/dylemma/xml/Matcher.scala
+++ core/src/main/scala/io/dylemma/xml/Matcher.scala
@@ -5,8 +5,8 @@ import io.dylemma.xml.IterateeHelpers.OpenTag
 trait Matcher[+A]{
 	def apply(input: OpenTag): Option[A]
 
-	def &[B, AB](that: Matcher[B])(implicit rc: ResultCombiner[A, B, AB]): Matcher[AB] = Matcher.combine(this, that)
-	def /[B, AB](that: Matcher[B])(implicit rc: ResultCombiner[A, B, AB]): ListMatcher[AB] = {
+	def &[B, AB](that: Matcher[B])(implicit rc: ContextCombiner[A, B, AB]): Matcher[AB] = Matcher.combine(this, that)
+	def /[B, AB](that: Matcher[B])(implicit rc: ContextCombiner[A, B, AB]): ListMatcher[AB] = {
 		ListMatcher.inductive(ListMatcher.single(this), ListMatcher.single(that))
 	}
 
@@ -18,7 +18,7 @@ trait Matcher[+A]{
 
 trait ListMatcher[+A]{
 	def apply(inputs: List[OpenTag]): Option[(A, List[OpenTag])]
-	def /[B, AB](next: Matcher[B])(implicit rc: ResultCombiner[A, B, AB]): ListMatcher[AB] = {
+	def /[B, AB](next: Matcher[B])(implicit rc: ContextCombiner[A, B, AB]): ListMatcher[AB] = {
 		ListMatcher.inductive(this, ListMatcher.single(next))
 	}
 
@@ -32,7 +32,7 @@ object Matcher {
 	def predicate(f: OpenTag => Boolean): Matcher[Unit] = new Matcher[Unit] {
 		def apply(elem: OpenTag) = if (f(elem)) Some(()) else None
 	}
-	def combine[A, B, AB](left: Matcher[A], right: Matcher[B])(implicit rc: ResultCombiner[A, B, AB]): Matcher[AB] = {
+	def combine[A, B, AB](left: Matcher[A], right: Matcher[B])(implicit rc: ContextCombiner[A, B, AB]): Matcher[AB] = {
 		new Matcher[AB] {
 			def apply(input: OpenTag) = for(a <- left(input); b <- right(input)) yield rc.combine(a, b)
 		}
@@ -48,62 +48,10 @@ object ListMatcher {
 	}
 
 	def inductive[A, B, AB](leading: ListMatcher[A], next: ListMatcher[B])(
-		implicit rc: ResultCombiner[A, B, AB]): ListMatcher[AB] = new ListMatcher[AB] {
+		implicit rc: ContextCombiner[A, B, AB]): ListMatcher[AB] = new ListMatcher[AB] {
 		def apply(inputs: List[OpenTag]) = for {
 			(leadMatch, leadTail) <- leading(inputs)
 			(nextMatch, nextTail) <- next(leadTail)
 		} yield rc.combine(leadMatch, nextMatch) -> nextTail
 	}
-}
-
-/** Typeclass to witness that a type `T` is *not* a Chain */
-final class NonChain[-T] private()
-object NonChain {
-	implicit def provideAnyNonChain[T] = new NonChain[T]
-	implicit def ambiguousChainNonChain[C <: Chain[_, _]] = new NonChain[C]
-}
-/** Typeclass to witness that a type `T` is *not* a Unit */
-final class NonUnit[-T] private()
-object NonUnit {
-	implicit def provideAnyNonUnit[T] = new NonUnit[T]
-	implicit val ambiguousUnitNonUnit1 = new NonUnit[Unit]
-	implicit val ambiguousUnitNonUnit2 = new NonUnit[Unit]
-}
-
-trait ResultCombiner[-A, -B, +AB]{
-	def combine(left: A, right: B): AB
-}
-object ResultCombiner {
-	import ChainSyntax._
-
-	private def makeCombiner[A, B, AB](f: (A, B) => AB): ResultCombiner[A, B, AB] = new ResultCombiner[A, B, AB] {
-		def combine(left: A, right: B) = f(left, right)
-	}
-
-	/* Note the use of the NonUnit and NonChain typeclasses here.
-	 * This is to disambiguate the generic cases like Any+Any where
-	 * there is a more specialized case like Chain+Chain.
-	 */
-
-	// Unit + T = T
-	implicit def provideUnitAnyCombiner[T]: ResultCombiner[Unit, T, T] = makeCombiner{ (u, t) => t }
-
-	// T + Unit = T
-	implicit def provideAnyUnitCombiner[T: NonUnit]: ResultCombiner[T, Unit, T] = makeCombiner{ (t, u) => t }
-
-	// A + B = A ~ B
-	implicit def provideAnyAnyCombiner[A: NonUnit: NonChain, B: NonUnit: NonChain]
-	: ResultCombiner[A, B, A ~ B] = makeCombiner{ _ ~ _ }
-
-	// A + C:Chain = A ~: C
-	implicit def provideAnyChainCombiner[A: NonUnit: NonChain, C <: Chain[_,_], R <: Chain[_, _]](
-		implicit prepender: ChainPrepend[A, C, R]): ResultCombiner[A, C, R] = makeCombiner{ _ ~: _ }
-
-	// This case *should* be covered by the AnyAnyCombiner
-	// C:Chain + A = C ~ A
-	implicit def provideChainAnyCombiner[C <: Chain[_, _], A: NonUnit: NonChain]: ResultCombiner[C, A, C ~ A] = makeCombiner{ _ ~ _ }
-
-	// A:Chain + B:Chain = A ++ B
-	implicit def provideChainChainCombiner[A <: Chain[_, _], B <: Chain[_, _], AB <: Chain[_, _]](
-		implicit concat: ChainConcat[A, B, AB]): ResultCombiner[A, B, AB] = makeCombiner{ _ ++ _ }
 }
