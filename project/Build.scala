9729e85f8d469f995bf205393f4650c244d33147
Dylan Halperin <dylan@dylemma.io>
2015-11-30 17:53:35 -0500

    Add source links for scaladoc

--- project/Build.scala
+++ project/Build.scala
@@ -1,3 +1,5 @@
+import scala.util.control.NonFatal
+
 import sbt._
 import Keys._
 import spray.boilerplate.BoilerplatePlugin.Boilerplate
@@ -15,10 +17,37 @@ object XmlStreamBuild extends Build {
 		.settings(libraryDependencies += "com.typesafe.play" %% "play-iteratees" % "2.4.3")
 		.settings(libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test")
 		.settings(Boilerplate.settings: _*)
+		.settings(apiDocSettings: _*)
 
 	lazy val examples = (project in file("examples"))
 		.settings(commonSettings: _*)
 		.dependsOn(core)
 
 	lazy val root = (project in file(".")).aggregate(core, examples)
+
+	lazy val snapshotBranch = {
+		try {
+			val branch = "git rev-parse --abbrev-ref HEAD".!!.trim
+			if(branch == "HEAD"){
+				"git rev-parse HEAD".!!.trim
+			} else branch
+		} catch {
+			case NonFatal(_) => "unknown"
+		}
+	}
+
+	lazy val apiDocSettings = Seq(
+		scalacOptions in (Compile, doc) <++= Def.task {
+			val version = Keys.version.value
+			val sourcePath = (baseDirectory in ThisBuild).value.getAbsolutePath
+			val sourceTree =
+				if(version endsWith "-SNAPSHOT") snapshotBranch
+				else version
+			val sourceUrl = "https://github.com/dylemma/xml-stream/tree/" + sourceTree + "\u20ac{FILE_PATH}.scala"
+			Seq(
+				"-sourcepath", sourcePath,
+				"-doc-source-url", sourceUrl
+			)
+		}
+	)
 }
