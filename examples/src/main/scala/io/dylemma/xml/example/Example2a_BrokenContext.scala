39b93b5cf73c6645fc629fdb641467240df7c2d7
Dylan Halperin <dylan@dylemma.io>
2016-07-09 18:20:15 -0400

    finish reworking and consolidating the examples

---
+++ examples/src/main/scala/io/dylemma/xml/example/Example2a_BrokenContext.scala
@@ -0,0 +1,38 @@
+package io.dylemma.xml.example
+
+import io.dylemma.xsp._
+import io.dylemma.xsp.syntax._
+
+object Example2a_BrokenContext extends App {
+
+	// note the <post id="D"> - we are calling `_.toInt` on this context value,
+	// which will throw an exception, which will end up wrapped in Error results
+	// by the parser
+	val rawXml = s"""<blog>
+		| <post id="D">
+		|  <comment user="bob">Hello there</comment>
+		|  <comment user="alice">Oh, hi</comment>
+		| </post>
+		| <post id="2">
+		|  <comment user="carla">Test comment!</comment>
+		|  <comment user="dylan">I'm testing too!</comment>
+		| </post>
+		|</blog>""".stripMargin
+
+	case class Comment(postId: Int, user: String, text: String)
+
+	implicit val CommentParser: Parser[Int, Comment] = Parser.combine(
+		Parser.forContext[Int],
+		Parser.forMandatoryAttribute("user"),
+		Parser.forText
+	).as(Comment)
+
+	val contextMatcher = "blog" \ ("post" & attr("id").map(_.toInt)) \ "comment"
+
+	val consumer = Splitter(contextMatcher)
+		.as[Comment] // parse the substreams created by the Splitter, using the implicit CommentParser
+		.expandResults // wrap inputs and errors as Results so we don't throw during the foreach
+		.consumeForEach(println) // println each of the results
+
+	consumer consume rawXml
+}
---
+++ examples/src/main/scala/io/dylemma/xml/example/Example4_ParserCombine.scala
@@ -0,0 +1,76 @@
+package io.dylemma.xml.example
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import io.dylemma.xsp._
+import io.dylemma.xsp.syntax._
+
+/**
+ * Created by dylan on 10/11/2015.
+ */
+object Example4_ParserCombine {
+
+	case class Comment(date: Date, user: User, stats: Stats, body: String)
+	case class User(id: String, name: String)
+	case class Stats(upvoteCount: Int, downvoteCount: Int)
+
+	val rawXml = """<comments>
+		|	<comment date="2014-07-20">
+		|		<user name="alice" id="98qja34j3"/>
+		|		<stats upvote-count="123" downvote-count="20"/>
+		|		<body>Parsing XML sure is a pain...</body>
+		|	</comment>
+		|	<comment date="2014-07-21">
+		|		<user name="bob" id="3adfngfl"/>
+		|		<stats upvote-count="10" downvote-count="2"/>
+		|		<body>Yeah, I use SAX, but it's still pretty tedious</body>
+		|	</comment>
+		|	<comment date="2014-07-21">
+		|		<user name="alice" id="98qja34j3"/>
+		|		<stats upvote-count="15" downvote-count="3"/>
+		|		<body>I tried StAX. It seems nicer, but still fairly repetitive</body>
+		|	</comment>
+		|	<comment date="2015-10-11">
+		|		<user name="dylemma" id="038oqfdje"/>
+		|		<stats upvote-count="23841" downvote-count="4"/>
+		|		<body>I made a new library to make XML parsing less awful! Try xml-stream, it'll change your life.</body>
+		|	</comment>
+		|</comments>""".stripMargin
+
+	/*
+	Note that we're marking the `UserParser` and `StatsParser` below as implicit.
+	This is simply for convenience so that we can use `Splitter(...).first[User]`
+	and `Splitter(...).first[Stats]` later on.
+	 */
+
+	// Parse <user id="..." name="..." /> into a User(id, name)
+	implicit val UserParser: Parser[Any, User] = Parser.combine(
+		Parser.forMandatoryAttribute("id"),
+		Parser.forMandatoryAttribute("name")
+	).as(User)
+
+	// Parse <stats upvote-count="..." downvote-count="..." /> into a Stats(upvoteCount, downvoteCount)
+	implicit val StatsParser: Parser[Any, Stats] = Parser.combine(
+		Parser.forMandatoryAttribute("upvote-count").map(_.toInt),
+		Parser.forMandatoryAttribute("downvote-count").map(_.toInt)
+	).as(Stats)
+
+	// note that SimpleDateFormat isn't thread-safe. You should use Joda time instead
+	val commentDateFormat = new SimpleDateFormat("yyyy-MM-dd")
+	// Parser for Comment
+	implicit val CommentParser: Parser[Any, Comment] = Parser.combine(
+		Parser.forMandatoryAttribute("date").map(commentDateFormat.parse),
+		Splitter(* \ "user").first[User],
+		Splitter(* \ "stats").first[Stats],
+		Splitter(* \ "body").first.asText
+	).as(Comment)
+
+	def main(args: Array[String]) {
+
+		val mainParser = Splitter("comments" \ "comment").as[Comment].consumeForEach(println)
+
+		mainParser consume rawXml
+	}
+
+}
